855bced7e16de5f64e7e2320fbd50047dc1f350e
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-19 19:28:25 +0200

    feat: get youtube videos from playlist

---
+++ src/getVideos.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export async function getVideos(accessToken: string) {
+  const rootURL = 'https://www.googleapis.com/youtube/v3/playlistItems'
+
+  const values = {
+    part: 'contentDetails',
+    maxResults: '10',
+    playlistId: 'PLogYAbXxpcswCx7liCyjv05nGPggNiLOh',
+  }
+
+  const qs = new URLSearchParams(values)
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }
+  const res = await axios.get(`${rootURL}?${qs.toString()}`, config)
+
+  return res.data
+}
