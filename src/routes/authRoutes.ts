34301d5a88644e53540970f41d04eb1382485a85
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-19 19:26:25 +0200

    feat: redirect route - get oauth tokens and save them to cookies

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,37 @@
 import { Request, Response } from 'express'
 import router from './router'
 import { getGoogleOAuthURL } from '../googleOAuthURL'
+import { getGoogleOAuthTokens } from '../getGoogleTokens'
 
 router.get('/auth', (req: Request, res: Response) => {
   res.json({
     generated: getGoogleOAuthURL(),
   })
+})
+
+router.get('/auth/google/redirect', async (req: Request, res: Response) => {
+  // get code from URL
+  const code = req.query.code as string
+
+  try {
+    const tokens = await getGoogleOAuthTokens(code)
+
+    res.cookie('access', tokens.access_token, {
+      httpOnly: true,
+    })
+
+    res.cookie('refresh', tokens.refresh_token, {
+      httpOnly: true,
+    })
+
+    res.json({
+      code: code,
+      tokens: tokens,
+    })
+
+    // res.redirect('/api/videos')
+  } catch (error: any) {
+    console.log(error.message)
+    res.redirect('/api/auth')
+  }
 })
