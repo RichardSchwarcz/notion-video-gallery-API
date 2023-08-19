df93423c5ac2470fde3bc8f01fe6e26a17d9f6d0
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-19 19:34:16 +0200

    feat: add error route for unauthorized access and provide options

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,10 +2,15 @@ import { Request, Response, Router } from 'express'
 
 const router = Router()
 
-router.get('/something', (req: Request, res: Response) => {
-  res.json({
-    message: 'something',
-  })
+router.get('/error/unauthorized', (req: Request, res: Response) => {
+  res.send(
+    `
+  <div>
+    <a href="/api/auth/refresh">Renew Session</a>
+    <a href="/api/auth">Log in again</a>
+  </div>
+  `
+  )
 })
 
 export default router
