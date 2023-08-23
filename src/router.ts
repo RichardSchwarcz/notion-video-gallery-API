3f8a966a8eaf06a017e8021dd91ef648060d00d8
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-23 14:57:16 +0200

    feat: define cookie validation middleware

--- src/router.ts
+++ src/router.ts
@@ -11,42 +11,42 @@ import { handleGetDbEntries } from './handlers/notionDatabaseHandler'
 
 const router = Router()
 
-// router.use(
-//   '/youtube/videos',
-//   (req: Request, res: Response, next: NextFunction) => {
-//     const cookieHeader = req.headers.cookie
-//     // console.log(cookieHeader)
-//     if (!cookieHeader || typeof cookieHeader !== 'string') {
-//       console.log('Cookie header is missing or not a string.')
-//       res.redirect('/api/youtube/auth')
-//       return
-//     }
+// cookies validation
+router.use(
+  '/youtube/videos',
+  (req: Request, res: Response, next: NextFunction) => {
+    const cookieHeader = req.headers.cookie
 
-//     const parsedCookies = parse(cookieHeader)
-//     const { access, refresh } = parsedCookies
+    if (!cookieHeader || typeof cookieHeader !== 'string') {
+      console.log('Cookie header is missing or not a string.')
+      res.redirect('/api/youtube/auth')
+      return
+    }
 
-//     if (!access || typeof access !== 'string') {
-//       console.log('Access token is missing or not a string.')
-//       res.redirect('/api/youtube/auth')
-//       return
-//     }
+    const parsedCookies = parse(cookieHeader)
+    const { access_token, refresh_token } = parsedCookies
 
-//     if (!refresh || typeof refresh !== 'string') {
-//       console.log('Refresh token is missing or not a string.')
-//       res.redirect('/api/youtube/auth')
-//       return
-//     }
+    if (!access_token || typeof access_token !== 'string') {
+      console.log('Access token is missing or not a string.')
+      res.redirect('/api/youtube/auth/refresh')
+      return
+    }
 
-//     next()
-//   }
-// )
+    if (!refresh_token || typeof refresh_token !== 'string') {
+      console.log('Refresh token is missing or not a string.')
+      res.redirect('/api/youtube/auth')
+      return
+    }
+
+    next()
+  }
+)
 
 // auth
 router.get('/youtube/auth', handleOAuthURL)
 
 router.get('/youtube/auth/redirect', handleGetOAuthTokens)
 
-// TODO handle refreshing access token in videos
 router.get('/youtube/auth/refresh', handleRefreshAccessToken)
 
 // youtube videos
