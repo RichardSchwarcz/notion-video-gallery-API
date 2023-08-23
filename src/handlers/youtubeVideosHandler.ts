010a7f1617f5e4d6f3c30b34858e5e1a3380c057
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-23 15:44:50 +0200

    chore: remove access token validation

--- src/handlers/youtubeVideosHandler.ts
+++ src/handlers/youtubeVideosHandler.ts
@@ -2,9 +2,6 @@ import { parse } from 'cookie'
 import { Request, Response } from 'express'
 import { fetchYoutubeVideos } from '../fetchYoutubeVideos'
 
-// ! REFACTOR: divide this function into  two functions. Cookie validation and Fetch
-// ! validate cookies inside of a middleware
-
 export async function handleGetVideos(req: Request, res: Response) {
   const cookieHeader = req.headers.cookie
 
@@ -16,12 +13,6 @@ export async function handleGetVideos(req: Request, res: Response) {
   const parsedCookies = parse(cookieHeader)
   const { access_token } = parsedCookies
 
-  if (!access_token || typeof access_token !== 'string') {
-    console.log('Access token cookie is missing or not a string.')
-    res.redirect('/api/youtube/auth')
-    return
-  }
-
   try {
     const videos = await fetchYoutubeVideos(access_token)
     res.json({
@@ -29,9 +20,6 @@ export async function handleGetVideos(req: Request, res: Response) {
     })
   } catch (error: any) {
     const errorMessage = `${error.response.status} ${error.response.statusText}`
-
-    // ! try res.get(error)
-    // res.status(404).send('Sorry, we cannot find that!') --> set status to 404 and send sorry
 
     if (errorMessage == '401 Unauthorized') {
       res.redirect('/api/error/unauthorized')
