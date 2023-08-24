3ac52e6fde810936d0b260924136c7b6a67b633e
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-24 15:23:47 +0200

    chore: return snippet instead of content details

--- src/fetchYoutubeVideos.ts
+++ src/fetchYoutubeVideos.ts
@@ -4,7 +4,7 @@ export async function fetchYoutubeVideos(accessToken: string) {
   const rootURL = 'https://www.googleapis.com/youtube/v3/playlistItems'
 
   const values = {
-    part: 'contentDetails',
+    part: 'snippet',
     maxResults: '10',
     playlistId: 'PLogYAbXxpcswCx7liCyjv05nGPggNiLOh',
   }
