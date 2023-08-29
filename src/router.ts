99156b65b93809c15fceabc685fafa5b4160f9c4
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-29 09:39:41 +0200

    fix: fix cookies middleware

--- src/router.ts
+++ src/router.ts
@@ -10,13 +10,7 @@ import {
   handleGetNotionVideos,
   handleInitialLoad,
 } from './handlers/notionDatabaseHandler'
-import {
-  fetchYoutubeVideos,
-  fetchYoutubeVideosRecursively,
-} from './fetchYoutubeVideos'
-import { getYoutubePlaylistInfo } from './getYoutubePlaylistInfo'
-import { postToNotionDatabase } from './postNotionEntries'
-import { postDelayedRequests } from './utils/postDelayedRequests'
+import { fetchYoutubeVideosRecursively } from './fetchYoutubeVideos'
 
 const router = Router()
 
@@ -41,18 +35,18 @@ router.use(
     // check access token
     if (!access_token || typeof access_token !== 'string') {
       console.log(
-        'Access token is missing or not a string. Redirecting to refresh from middleware'
+        'Access token is missing or not a string. Checking for refresh token'
       )
-      res.redirect('/api/youtube/auth/refresh')
-      return
-    }
-
-    // check refresh token
-    if (!refresh_token || typeof refresh_token !== 'string') {
-      console.log(
-        'Refresh token is missing or not a string. Redirecting to auth from middleware'
-      )
-      res.redirect('/api/youtube/auth')
+      // check refresh token
+      if (!refresh_token || typeof refresh_token !== 'string') {
+        console.log(
+          'Refresh token is missing or not a string. Redirecting to auth from middleware'
+        )
+        res.redirect('/api/youtube/auth')
+        return
+      } else {
+        res.redirect('/api/youtube/auth/refresh')
+      }
       return
     }
 
@@ -71,6 +65,27 @@ router.get('/youtube/auth/refresh', handleRefreshAccessToken)
 // youtube videos
 // ! add restricted middleware. no post request allowed without access token
 router.get('/youtube/videos', handleGetYoutubeVideos)
+
+// router.get('/youtube/durations', async (req: Request, res: Response) => {
+//   const cookieHeader = req.headers.cookie
+
+//   if (!cookieHeader || typeof cookieHeader !== 'string') {
+//     console.log('Cookie header is missing or not a string.')
+//     return
+//   }
+
+//   const parsedCookies = parse(cookieHeader)
+//   const { access_token } = parsedCookies
+
+//   try {
+//     // fetch duration of videos
+//     const videos = await fetchYoutubeVideosRecursively(access_token, undefined)
+
+//     res.json({ allVideos: videos })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
 // notion database
 router.get('/notion/videos', handleGetNotionVideos)
