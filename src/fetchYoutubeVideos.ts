0905c0b2c8373c94e3ab1363f7ca9ca30bc24206
riso.schwarcz <riso.schwarcz@gmail.com>
2023-08-30 09:11:02 +0200

    refactor: rebuild fetching functions

--- src/fetchYoutubeVideos.ts
+++ src/fetchYoutubeVideos.ts
@@ -1,16 +1,21 @@
 import axios from 'axios'
-import { videoSchema } from './postNotionEntries'
+import { FetchVideosOptions } from './types/videoTypes'
 
-export async function fetchYoutubeVideos(accessToken: string) {
-  const rootURL = 'https://www.googleapis.com/youtube/v3/playlistItems'
+const ENDPOINT = {
+  playlistItems: 'playlistItems',
+  videos: 'videos',
+} as const
 
-  const values = {
-    part: 'snippet',
-    maxResults: '10',
-    playlistId: 'PLogYAbXxpcswCx7liCyjv05nGPggNiLOh',
-  }
+type Endpoint = keyof typeof ENDPOINT
 
-  const qs = new URLSearchParams(values)
+// ------------- FUNCTIONS ----------------
+
+export async function fetchYoutubeVideos(
+  accessToken: string,
+  endpoint: Endpoint,
+  qs: URLSearchParams
+) {
+  const rootURL = `https://www.googleapis.com/youtube/v3/${endpoint}`
 
   const config = {
     headers: {
@@ -22,78 +27,83 @@ export async function fetchYoutubeVideos(accessToken: string) {
   return res.data
 }
 
-type FetchPlaylistOptions = {
-  part: string
-  maxResults: string
-  playlistId: string
-  pageToken?: string
-}
+// ------------------------------------------
 
-function generateQueryString(options: FetchPlaylistOptions) {
+export function generateQueryString(
+  options: FetchVideosOptions
+): URLSearchParams {
   const commonOptions = {
     part: options.part,
     maxResults: options.maxResults,
-    playlistId: options.playlistId,
   }
 
-  if (options.pageToken !== undefined) {
-    return new URLSearchParams({
+  let queryStringParams
+
+  if (options.id) {
+    queryStringParams = {
       ...commonOptions,
+      id: options.id,
+    }
+  }
+
+  if (options.playlistId) {
+    queryStringParams = {
+      ...commonOptions,
+      playlistId: options.playlistId,
+    }
+  }
+
+  if (!options.id && !options.playlistId) {
+    throw new Error('Error : id or playlistId must be provided')
+  }
+
+  if (options.pageToken) {
+    return new URLSearchParams({
+      ...queryStringParams,
       pageToken: options.pageToken,
     })
   }
 
-  return new URLSearchParams(commonOptions)
+  return new URLSearchParams(queryStringParams)
 }
+
+// --------------------------------------------
 
 export async function fetchYoutubeVideosRecursively(
   accessToken: string,
-  nextPageToken: string | undefined,
-  allVideos: videoSchema[] = []
+  endpoint: Endpoint,
+  options: FetchVideosOptions,
+  nextPageToken: string | undefined = undefined,
+  allVideos: any[] = []
 ) {
-  const rootURL = 'https://www.googleapis.com/youtube/v3/playlistItems'
-
-  const options = {
-    part: 'snippet',
-    maxResults: '10',
-    playlistId: 'PLogYAbXxpcswCx7liCyjv05nGPggNiLOh',
+  const options_ = {
+    ...options,
     pageToken: nextPageToken,
   }
 
-  const qs = generateQueryString(options)
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  }
-
   try {
-    const res = await axios.get(`${rootURL}?${qs.toString()}`, config)
-    const items = res.data.items || []
+    const qs = generateQueryString(options_)
+
+    const data = await fetchYoutubeVideos(accessToken, endpoint, qs)
+    const items = data.items || []
 
     items.forEach((item: any) => {
-      const videoObject: videoSchema = {
-        title: item.snippet.title,
-        // description: item.snippet.description,
-        thumbnail: item.snippet.thumbnails.high.url,
-        url: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
-        videoOwnerChannelTitle: item.snippet.videoOwnerChannelTitle,
-      }
-      allVideos.push(videoObject)
+      allVideos.push(item)
     })
 
-    if (res.data.nextPageToken) {
+    if (data.nextPageToken) {
       return fetchYoutubeVideosRecursively(
         accessToken,
-        res.data.nextPageToken,
+        endpoint,
+        options_,
+        data.nextPageToken,
         allVideos
       )
     }
 
     return allVideos
   } catch (error) {
-    console.log('Error fetching playlist items')
+    console.log('Error fetching youtube videos')
     throw error
   }
 }
