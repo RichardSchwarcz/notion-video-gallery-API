b30e8a9de3892a80fd6ec85c94f5c68fb41cc8ff
riso.schwarcz <riso.schwarcz@gmail.com>
2023-09-01 15:33:17 +0200

    feat: get playlist items IDs to be deleted

--- src/handlers/sync.ts
+++ src/handlers/sync.ts
@@ -1,16 +1,53 @@
 import { Request, Response } from 'express'
 import { getNotionDatabaseItems } from '../getNotionVideos'
 import { compareArrays } from '../utils/compareArrays'
+import {
+  PageObjectResponse,
+  QueryDatabaseResponse,
+} from '@notionhq/client/build/src/api-endpoints'
+import { getNotionDataIDs } from '../utils/notionHelpers'
+import { SnapshotData } from '../types/notionTypes'
+import { getYoutubeVideoIDfromURL } from '../utils/youtubeHelpers'
 
 export async function sync(req: Request, res: Response) {
   // get notion videos (Ids)
   const database_id = process.env.NOTION_DATABASE_ID as string
-  const notionVideosIDs = await getNotionDatabaseItems(database_id)
+  const notionMainData = await getNotionDatabaseItems(database_id)
+  const notionMainDataIDs = getNotionDataIDs(notionMainData)
+
   // get notion snapshot (Ids)
   const snapshot_id = process.env.NOTION_SNAPSHOT_ID as string
-  const notionSnapshotIds = await getNotionDatabaseItems(snapshot_id)
+  const notionSnapshotData = await getNotionDatabaseItems(snapshot_id)
+  const notionSnapshotDataIDs = getNotionDataIDs(notionSnapshotData)
+
   // compare videos and snapshot = see which videos have been deleted
-  const difference = compareArrays(notionVideosIDs, notionSnapshotIds)
+  const difference: string[] = compareArrays(
+    notionMainDataIDs,
+    notionSnapshotDataIDs
+  )
+
+  // get video ID as playlist item - each video in playlist has its own unique ID for that playlist
+  const getPlaylistItemsIDs = (
+    difference: string[],
+    notionSnapshotData: any
+  ) => {
+    if (difference.length > 0) {
+      const toDelete = notionSnapshotData.results.filter((video: any) => {
+        const URL = video.properties.URL.url
+        const ID = getYoutubeVideoIDfromURL(URL)
+        if (ID) {
+          return difference.includes(ID)
+        }
+        return
+      })
+
+      const playlistItemsIDs = toDelete.map((video: any) => {
+        return video.properties.PlaylistItemID.rich_text[0].text.content
+      })
+      return playlistItemsIDs
+    }
+  }
+
   // post to youtube playlist (remove deleted videos from notion)
   //.....
   // get all youtube videos (video objects)
@@ -22,8 +59,9 @@ export async function sync(req: Request, res: Response) {
   // post to notion snapshot
 
   res.json({
-    difference: difference,
-    main: notionVideosIDs,
-    snapshot: notionSnapshotIds,
+    deletedVideos: difference,
+    toDelete: getPlaylistItemsIDs(difference, notionSnapshotData),
+    main: notionMainDataIDs,
+    snapshot: notionSnapshotData,
   })
 }
