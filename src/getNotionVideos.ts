7940e12ba4c3c77ef37d2f7d3f8dd0827b2ce4ae
riso.schwarcz <riso.schwarcz@gmail.com>
2023-09-01 15:25:26 +0200

    refactor: return items not ids

--- src/getNotionVideos.ts
+++ src/getNotionVideos.ts
@@ -1,8 +1,5 @@
 import { Client } from '@notionhq/client'
-import {
-  PageObjectResponse,
-  QueryDatabaseParameters,
-} from '@notionhq/client/build/src/api-endpoints'
+import { QueryDatabaseParameters } from '@notionhq/client/build/src/api-endpoints'
 
 const notion = new Client({ auth: process.env.NOTION_SECRET })
 
@@ -12,16 +9,5 @@ export async function getNotionDatabaseItems(database_id: string) {
   const params: QueryDatabaseParameters = {
     database_id: database_id,
   }
-  const res = await notion.databases.query(params)
-  const URLs = res.results.map((video: any) => {
-    return video.properties.URL.url
-  })
-  const IDs = URLs.map((url) => {
-    const regex = /(?:v=|\/)([a-zA-Z0-9_-]{11})/
-    const match = url.match(regex)
-    if (match) {
-      return match[1]
-    }
-  })
-  return IDs
+  return notion.databases.query(params)
 }
